f65fcee77200ab14c8d00d8777901a10be61038a
Audrey Schmidt <audsch@outlook.com>
2022-01-13 09:18:41 -0600

    refined navbar styling

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,33 +2,33 @@ const NavBar = (props) => {
     return ( 
         <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#">AUDREY SCHMIDT</a>
+            <a className="navbar-brand" href="#" style={{'marginTop':'-1.2em'}}>AUDREY SCHMIDT</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-                <li className="nav-item">
+            <ul className="navbar-nav" >
+                <li className="nav-item" style={{'marginTop':'-1em'}}>
                 <a className="nav-link" aria-current="page" href="#homepage">HOME</a>
                 </li>
-                <li className="nav-item">
+                <li className="nav-item" style={{'marginTop':'-1em'}}>
                 <a className="nav-link" href="#about-me">ABOUT ME</a>
                 </li>
-                <li className="nav-item">
+                <li className="nav-item" style={{'marginTop':'-1em'}}>
                 <a className="nav-link" href="#resume">RESUME/CV</a>
                 </li>
-                <li className="nav-item dropdown">
+                <li className="nav-item dropdown" style={{'marginTop':'-1em'}}>
                 <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     PROJECTS
                 </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink" style={{'marginTop':'1.5em'}}>
                     <li><a className="dropdown-item" href="#projects">Home</a></li>
                     <li><a className="dropdown-item" href="#capstone">Capstone</a></li>
                     <li><a className="dropdown-item" href="#group">Group</a></li>
                     <li><a className="dropdown-item" href="#individual">Individual</a></li>
                 </ul>
                 </li>
-                <li className="nav-item">
+                <li className="nav-item" style={{'marginTop':'-1em'}}>
                 <a className="nav-link" href="#contact">CONTACT ME</a>
                 </li>
             </ul>
