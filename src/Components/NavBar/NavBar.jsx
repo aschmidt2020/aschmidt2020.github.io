3ac833946eb474a2d59985957dd6415b0bf8a107
Audrey Schmidt <audsch@outlook.com>
2022-01-13 14:42:50 -0600

    updated nav links to correct hrefs

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -13,20 +13,20 @@ const NavBar = (props) => {
 
             <ul className="navbar-nav">
                 <Scrollspy items={ ['section-1', 'section-2', 'section-3', 'section-4', 'section-5'] } currentClassName="is-current">
-                    <li className="nav-item"><a className="nav-link" aria-current="page" href="#section-1">HOME</a></li>
-                    <li className="nav-item"><a className="nav-link" aria-current="page" href="#section-2">ABOUT ME</a></li>
-                    <li className="nav-item"><a className="nav-link" aria-current="page" href="#section-3">RESUME/CV</a></li>
+                    <li className="nav-item"><a className="nav-link" aria-current="page" href="#homepage">HOME</a></li>
+                    <li className="nav-item"><a className="nav-link" aria-current="page" href="#about-me">ABOUT ME</a></li>
+                    <li className="nav-item"><a className="nav-link" aria-current="page" href="#resume">RESUME/CV</a></li>
                     <li className="nav-item dropdown"><a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         PROJECTS
                         </a>
                         <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink" style={{'marginTop':'0.5em'}}>
-                            <li><a className="dropdown-item" href="#section-4">Home</a></li>
+                            <li><a className="dropdown-item" href="#projects">Home</a></li>
                             <li><a className="dropdown-item" href="#capstone">Capstone</a></li>
                             <li><a className="dropdown-item" href="#group">Group</a></li>
                             <li><a className="dropdown-item" href="#individual">Individual</a></li>
                         </ul>
                     </li>
-                    <li className="nav-item"><a className="nav-link" aria-current="page" href="#section-5">CONTACT ME</a></li>
+                    <li className="nav-item"><a className="nav-link" aria-current="page" href="#contact">CONTACT ME</a></li>
                 </Scrollspy>
             </ul>
             </div>
