1a1ff400a83866ca99cbeff48915a8fbb767d5ef
Audrey Schmidt <audsch@outlook.com>
2022-01-13 10:50:17 -0600

    Added carousel to project homepage

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -4,16 +4,6 @@ import capstone from '../../Images/CAPSTONEfarzad-nazifi-p-xSl33Wxyc-unsplash.jp
 
 const Projects = (props) => {
 
-    function handleClickGroup(event) {
-        event.preventDefault();
-        props.toggleGroupVisible()
-    }
-
-    function handleClickIndiv(event) {
-        event.preventDefault();
-        props.toggleIndividualVisible()
-    }
-
     return (
         <div className = "container-fluid project-page" id = "projects">
             <br></br><br></br>
@@ -27,7 +17,7 @@ const Projects = (props) => {
                 <div className="col-4 project-box">
                     <div className="item-end-left">
                         <h1 className = "projects-bold">CAPSTONE</h1>
-                        <img src ={capstone} alt = "dual monitor computer setup" className = "project-picture-size img-responsive center-block" />
+                        <img src ={capstone} alt = "dual monitor computer setup" className = "img-responsive d-block w-100" style={{'height':'261px', 'width':'391px'}} />
                         <h2 className = "about-me-bold-left">Capstone Project Name</h2>
                         <p className = "about-me-normal" style={{'marginTop':'-1em'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                         <p className = "view-proj-link"><a href = "#capstone" className = "view-proj-link">view <i class="bi bi-arrow-right"></i></a></p>    
@@ -36,12 +26,53 @@ const Projects = (props) => {
 
                 <div className="col-4 project-box">
                     <div className='item-middle'>
-
                         <h1 className = "projects-bold">GROUP</h1>
-                        <img src ={group} alt = "group learning and project work" className = "project-picture-size img-responsive center-block" />
+                        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+                        <div className="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                            <img src ={group} alt = "group learning and project work" className = "img-responsive d-block w-100" style={{'height':'261px', 'width':'391px'}}/>
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                                <button href = "#" className = "view-proj-link btn btn-secondary">view <i class="bi bi-arrow-right"></i></button>
+                            </div>
+                            </div>
+                            <div className="carousel-item">
+                            <img src ={group} alt = "group learning and project work" className = "img-responsive d-block w-100" style={{'height':'261px', 'width':'391px'}}/>
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Second slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                                <button href = "#" className = "view-proj-link btn btn-secondary">view <i class="bi bi-arrow-right"></i></button>
+                            </div>
+                            </div>
+                            <div className="carousel-item">
+                            <img src ={group} alt = "group learning and project work" className = "img-responsive d-block w-100" style={{'height':'261px', 'width':'391px'}}/>
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Third slide label</h5>
+                                <p>Some representative placeholder content for the first slide. </p>
+                                <button href = "#" className = "view-proj-link btn btn-secondary">view <i class="bi bi-arrow-right"></i></button>  
+                            </div>
+                            </div>
+                        </div>
+                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Previous</span>
+                        </button>
+                        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Next</span>
+                        </button>
+                        </div>
+
+                        
                         <h2 className = "about-me-bold-left"> Group Projects</h2>
                         <p className = "about-me-normal" style={{'marginTop':'-1em'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <p className = "view-proj-link"><button onClick={handleClickGroup} href = "#group" className = "view-proj-link">view <i class="bi bi-arrow-right"></i></button></p>   
+                        <br></br>
                     </div>
                 </div>
 
@@ -49,10 +80,51 @@ const Projects = (props) => {
                     <div className='item-end-right'>
 
                         <h1 className = "projects-bold">INDIVIDUAL</h1>
-                        <img src ={individual} alt = "code" className = "project-picture-size img-responsive center-block" />
+                        <div id="carouselExampleIndicators2" className="carousel slide" data-bs-ride="carousel">
+                        <div className="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                            <img src ={individual} alt = "group learning and project work" className = "img-responsive d-block w-100" style={{'height':'261px', 'width':'391px'}}/>
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                                <button href = "#" className = "view-proj-link btn btn-secondary">view <i class="bi bi-arrow-right"></i></button>
+                            </div>
+                            </div>
+                            <div className="carousel-item">
+                            <img src ={individual} alt = "group learning and project work" className = "img-responsive d-block w-100" style={{'height':'261px', 'width':'391px'}}/>
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Second slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                                <button href = "#" className = "view-proj-link btn btn-secondary">view <i class="bi bi-arrow-right"></i></button>
+                            </div>
+                            </div>
+                            <div className="carousel-item">
+                            <img src ={individual} alt = "group learning and project work" className = "img-responsive d-block w-100" style={{'height':'261px', 'width':'391px'}}/>
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Third slide label</h5>
+                                <p>Some representative placeholder content for the first slide. </p>
+                                <button href = "#" className = "view-proj-link btn btn-secondary">view <i class="bi bi-arrow-right"></i></button>  
+                            </div>
+                            </div>
+                        </div>
+                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Previous</span>
+                        </button>
+                        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Next</span>
+                        </button>
+                        </div>
+                        
                         <h2 className = "about-me-bold-left"> Individual Projects</h2>
                         <p className = "about-me-normal" style={{'marginTop':'-1em'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <p className = "view-proj-link"><button onClick={handleClickIndiv} href = "#individual" className = "view-proj-link">view <i class="bi bi-arrow-right"></i></button></p>   
+                        <br></br>
                     </div>
                 </div>
             </div>
